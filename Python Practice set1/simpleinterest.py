37c6344d4ca67c555cf1a0f320685605756da15e
Sanket Arali <110754364+Sanketarali@users.noreply.github.com>
2023-01-28 09:09:20 +0530

    Delete simpleinterest.py

--- Python Practice set1/simpleinterest.py
+++
@@ -1,5 +0,0 @@
-p=float(input("Enter the profit\n"))
-t=float(input("Enter the tax\n"))
-r=float(input("Entter the rate of interest\n"))
-simple_interset=(p*t*r)/100
-print("The simple interest is ",simple_interset)
